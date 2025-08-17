4eb617eab0fb1354164f9ee7da75ad8940526841
Gaurav Singh Bisht <bisht.gaurav808@gmsil.com>
2025-08-17 17:43:09 +0530

    firebase changes

--- src/lib/store.ts
+++ src/lib/store.ts
@@ -6,12 +6,12 @@ import type { Category } from "../lib/core";
 import { DEFAULT_DATA, uid } from "../lib/core";
 
 const FIREBASE_CONFIG = {
-  apiKey: (import.meta as any).env?.VITE_FIREBASE_API_KEY || "",
-  authDomain: (import.meta as any).env?.VITE_FIREBASE_AUTH_DOMAIN || "",
-  projectId: (import.meta as any).env?.VITE_FIREBASE_PROJECT_ID || "",
-  storageBucket: (import.meta as any).env?.VITE_FIREBASE_STORAGE_BUCKET || "",
-  messagingSenderId: (import.meta as any).env?.VITE_FIREBASE_MESSAGING_SENDER_ID || "",
-  appId: (import.meta as any).env?.VITE_FIREBASE_APP_ID || "",
+  apiKey: "AIzaSyCi96DII_BSsZfn8FsdGkjTcUbYrwd_Yf4",
+  authDomain:  "week-gaols.firebaseapp.com",
+  projectId: "week-gaols",
+  storageBucket: "week-gaols.firebasestorage.app",
+  messagingSenderId: "213614122894",
+  appId: "1:213614122894:web:aa7c849086047a24ebb6df",
 };
 const hasFirebaseConfig = () => !!(FIREBASE_CONFIG.apiKey && FIREBASE_CONFIG.projectId);
 
